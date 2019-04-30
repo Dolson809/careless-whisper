8dff6f04cb98351ee3f5677dc76d05e7b223dfc5
Jonathan Viera <go_chunkymonkey@yahoo.com>
2019-04-30 11:24:16 -0700

    pushing new files to branch

---
+++ careless-whisper/src/Components/Whispers/Create/CreateWhisper.styles.js
@@ -0,0 +1,16 @@
+export default theme => ({
+        container: {
+          display: 'flex',
+          flexWrap: 'wrap',
+        },
+        textField: {
+          marginLeft: theme.spacing.unit,
+          marginRight: theme.spacing.unit,
+        },
+        dense: {
+          marginTop: 16,
+        },
+        menu: {
+          width: 200,
+        },
+  });
